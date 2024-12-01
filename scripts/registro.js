43554d74305cf173ccabd1552abe2cb40641178a
FacundoTF <facufarneda@gmail.com>
2024-12-01 12:35:20 -0300

    Campos para la retroalimentación agregados

--- scripts/registro.js
+++ scripts/registro.js
@@ -3,13 +3,10 @@ var AlertaModal = document.getElementById("AlertaModal")
 document.querySelector("#BotonInicio").addEventListener("click", (event) => {
     var nombre = document.querySelector("#EntradaNombre").value
     var nombreValido = validarNombre(nombre)
-
-    debugger
     if(nombreValido === false){
         AlertaModal.style.display = "flex"
         return
     }
-
     sessionStorage.setItem("Jugador", nombre)
     window.open("/html/juego.html", "_self",)//En el primer parametro debe ir a la pagina del juego
 })
